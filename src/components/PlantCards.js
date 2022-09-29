e246455dc9a90e969006550b38f1930f53b6eb2a
Jordan Jennings <jordanjennings12@gmail.com>
2022-09-29 19:43:29 -0400

    first commit

---
+++ src/components/PlantCards.js
@@ -0,0 +1,9 @@
+export default function PlantCards() {
+    return (
+        <div>
+            <h3>Plant Name</h3>
+            <img src="https://i.gifer.com/5FC7.gif" alt="plant"/>
+            <button>Add Plant</button>
+        </div>
+    )
+}
