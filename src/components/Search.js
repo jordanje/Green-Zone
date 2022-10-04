c058d9b7c5866bb066bfe4653cf43be2bca580f3
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-04 16:39:40 -0400

    navbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import "./Search.css"
 
 function Search({handleSearchChange, searchValue}) {
 
 
     return (
-        <div>
-            <form className="search-plants">
+        <div className="search">
+            <form className="search-bar">
                 <input type="text" placeholder="Search plants..." value={searchValue} onChange={handleSearchChange}/>
             </form>
         </div>
