f7cf3bd90066b2d882bbbae86601c6a36aa8e123
Jordan Jennings <jordanjennings12@gmail.com>
2022-10-03 23:03:10 -0400

    changes to rooms

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react"
+import "./Room.css"
+
 export default function Room({room, addedPlants}) {
-    console.log(addedPlants)
+    const [ plants, setPlants ] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/rooms/${room.id}/added_plants`)
+        .then(data => data.json())
+        .then(plants => setPlants(plants))
+    })
+
     return (
-        <div>
+        <div id="room">
             <h2>{room.name}</h2>
-            <p></p>
+            <div className="added-plants">
+            {plants.map((plant) => (
+            <div>
+                <p>{plant.name}</p>
+                <img src={plant.image} />
+            </div>
+            ))}
+            </div>
         </div>
     )
 }
--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -2,15 +2,15 @@ import "./RoomContainter.css"
 import Room from "./Room"
 
 export default function RoomContainer({rooms, addedPlants}) {
-    console.log(rooms)
+
     return (
         <div id="room-container">
             <style>{'body { background-color: rgb(231, 222, 210); }'}</style>
             <div className="rooms">
                  {rooms.map((room) => {
                     return (
-                    <div className="room">
-                        <Room room={room} addedPlants={addedPlants}/>
+                    <div className="room" id={room.id}>
+                        <Room key={room.id} room={room} addedPlants={addedPlants}/>
                     </div>
                     )
                  }
